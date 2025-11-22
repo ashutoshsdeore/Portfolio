decf3e6a5fceada95fa2cca1f1285d147797209a
ashutoshsdeore <deoreashutosh6@gmail.com>
2025-11-22 12:56:45 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Test() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-indigo-600">
+        Tailwind is Working! 🎉
+      </h1>
+    </div>
+  );
+}
